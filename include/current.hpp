17e4371c5f8c6040e1829e02c7cc9d327a1009ca
Shin Umeda <umeda.shin@gmail.com>
2024-08-20 20:22:24 -0700

    fix adc prescaler frequency

--- include/current.hpp
+++ include/current.hpp
@@ -13,7 +13,9 @@ inline void init_adc() {
 inline float get_analog(uint8_t pin) {
   ADMUX &= ~0x0f;
   ADMUX |= (pin & 0x0f);
-  ADCSRA |= setmask(ADSC);
+
+  // prescaler is 1/32
+  ADCSRA |= setmask(ADSC, ADPS2, ADPS0);
   loop_until_bit_is_set(ADCSRA, ADIF);
   uint16_t result = ADCL | (ADCH << 8);
   return result * 5;
