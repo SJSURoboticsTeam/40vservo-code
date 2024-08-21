cec39e0d3ef6f81e64ad5cccec97ac4ecf040ff5
Shin Umeda <umeda.shin@gmail.com>
2024-08-20 20:24:38 -0700

    main now uses a timer to sleep when not doing work

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include "i2c.hpp"
 #include "pwm.hpp"
 #include "rotation.hpp"
+#include "timer.hpp"
 
 namespace {
 constexpr uint8_t ipropi_pin = 0;
@@ -108,19 +109,25 @@ int main() {
   init_tmag();
   init_adc();
   init_pwm();
+  init_timer();
   DeviceState state;
   // i2c_state = &i2c;
   state.update_loc(get_angle());
   state.update_loc(get_angle());
   state.set_current(get_analog(ipropi_pin));
   init_i2c();
+  sei();
   // todo change to timer solution later
   while (true) {
+    sleep_cpu();
+    if (ticks < 14) {
+      continue;
+    }
+    ticks = 0;
     cli();
     set_motor(state.get_output());
     state.update_loc(get_angle());
     state.set_current(get_analog(ipropi_pin));
     sei();
-    _delay_ms(200);
   }
 }
